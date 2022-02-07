2f5830a0f809707adc0e90cb0020767bd2def89f
Amol <amol_s@outlook.com>
2022-02-07 12:51:54 -0800

    V2

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/amols/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/amols/keymap.c
@@ -4,46 +4,67 @@
 
 
 #define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
+#define _L1 1
+#define _L2 2
+#define _MAC 3
 
-#define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
+#define L1 MO(_L1)
+#define L2 MO(_L2)
+#define MAC TG(_MAC)
+
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
-     KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-                      KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
-                                      RAISE,KC_SPC,                        KC_ENT, LOWER,
-                                      KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
-                                      KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT
+     KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,       KC_MINS,
+     KC_LALT, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,       KC_BSLS,
+     KC_LEAD, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,       KC_QUOT,
+     LCA(KC_DEL), KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,LSFT(KC_MINS), KC_EQL,
+                      RCS(KC_TAB),C(KC_TAB),                                                       S(KC_TAB), KC_TAB,
+                                      LGUI_T(KC_TAB),LSFT_T(KC_ESC),        KC_SPC, KC_ENT,
+                                      L2,     L1,                           KC_BSPC,  KC_LCTL,
+                                      MAC, KC_LCTL,                          LSFT(KC_SCLN), KC_SLASH
   ),
 
-  [_LOWER] = LAYOUT_5x6(
-
-     KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-     _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
-     _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-     _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+  [_L1] = LAYOUT_5x6(
+     _______,_______, _______ ,_______,_______ ,_______,                        _______,_______,_______,_______,_______,_______,
+     _______,_______,_______,S(KC_9),S(KC_0),_______,                        _______, _______ , _______ , _______ ,_______,_______,
+     _______,_______,_______,KC_LBRC,KC_RBRC ,_______,                        KC_LEFT, KC_DOWN , KC_UP , KC_RIGHT, KC_MINS, KC_PIPE,
+     _______,_______,_______,S(KC_LBRC),S(KC_RBRC), _______,                  KC_HOME, KC_PGDN , KC_PGUP , KC_END ,KC_EQL ,KC_UNDS,
                                              _______,KC_PSCR,            _______, KC_P0,
                                              _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______,
+                                             _______,_______,            KC_DEL,_______,
                                              _______,_______,            _______,_______
 
   ),
 
-  [_RAISE] = LAYOUT_5x6(
-       KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-       _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
-       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
-                                               _______,_______,            KC_EQL ,_______,
-                                               _______,_______,            _______,_______,
-                                               _______,_______,            _______,_______,
-                                               _______,_______,            _______,_______
+  [_L2] = LAYOUT_5x6(
+     _______,_______, _______ ,_______,_______ ,_______,                        _______,_______,_______,_______,_______,_______,
+     _______,_______,_______,_______,_______,_______,                        _______, _______ , _______ , _______ ,_______,_______,
+     _______,_______,_______,_______, _______ ,_______,                  C(KC_LEFT), KC_DOWN , KC_UP , C(KC_RIGHT), KC_MINS, KC_PIPE,
+     _______,_______,_______,_______, _______, _______,                  KC_HOME, KC_PGDN , KC_PGUP , KC_END ,KC_EQL ,KC_UNDS,
+                                             _______,KC_PSCR,            _______, KC_P0,
+                                             _______,_______,            _______,_______,
+                                             _______,_______,            KC_DEL,_______,
+                                             _______,_______,            _______,_______
+  ),
+
+  [_MAC] = LAYOUT_5x6(
+     KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,       KC_MINS,
+     KC_LALT, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,       KC_BSLS,
+     KC_LEAD, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,       KC_QUOT,
+     C(KC_Z), KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,LSFT(KC_MINS), KC_EQL,
+                      RCS(KC_TAB),C(KC_TAB),                                                       C(KC_LEFT), C(KC_RIGHT),
+                                      LCTL_T(KC_TAB),LSFT_T(KC_ESC),        KC_SPC, KC_ENT,
+                                      L2,     L1,                           KC_BSPC,  KC_LCTL,
+                                      _______, KC_LGUI,                          LSFT(KC_SCLN), KC_SLASH
   ),
 };
+
+// const key_override_t alt_f4_override = ko_make_basic(MOD_MASK_CTRL, KC_Q, A(KC_F4));
+// // This globally defines all key overrides to be used
+// const key_override_t **key_overrides = (const key_override_t *[]){
+//     &alt_f4_override,
+//     NULL // Null terminate the array of overrides!
+// };
